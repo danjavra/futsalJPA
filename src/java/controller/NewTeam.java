21eaf032e68200692b7f417bac27e443e116006d
asg97 <serrano.ag6@gmail.com>
2018-05-15 22:29:15 +0200

    New equipo and html correspondiente creados

---
+++ src/java/controller/NewTeam.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.FutsalDAO;
+import entities.Equip;
+import entities.Usuari;
+import excepciones.MiExcepcion;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Anjo
+ */
+public class NewTeam extends HttpServlet {
+
+    FutsalDAO futsalDAO = new FutsalDAO();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, MiExcepcion {
+        String nombreUsuario = request.getParameter("username");
+        String password = request.getParameter("contrasenya");
+        Integer tipo = Integer.parseInt(request.getParameter("tipo"));
+        String nom = request.getParameter("nom");
+        String ciutat = request.getParameter("ciutat");
+        Integer telefon = Integer.parseInt(request.getParameter("telefon"));
+        Integer punts = Integer.parseInt(request.getParameter("punts"));
+
+        Usuari u = new Usuari(nombreUsuario, password, tipo);
+        Equip e = new Equip(nom, ciutat, telefon, punts,u);
+
+        try {
+            System.out.println(u.getContrasenya());
+            futsalDAO.insertarUser(u);
+            futsalDAO.insertarTeam(e);
+            request.setAttribute("success", "New Team Registered");
+        } catch (MiExcepcion ex) {
+            request.setAttribute("error", ex.getMessage());
+        }
+        request.getRequestDispatcher("/estadoAltaJEquipo.jsp").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(NewPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MiExcepcion ex) {
+            Logger.getLogger(NewPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(NewPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MiExcepcion ex) {
+            Logger.getLogger(NewPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/dao/FutsalDAO.java
+++ src/java/dao/FutsalDAO.java
@@ -23,12 +23,12 @@ import java.sql.Statement;
  * @author Anjo
  */
 public class FutsalDAO {
-    
+
     private Connection conexion;
-    
-      // ********************* Inserts ****************************
-public void insertarUser(Usuari u) throws SQLException, MiExcepcion {
-    conectar();
+
+    // ********************* Inserts ****************************
+    public void insertarUser(Usuari u) throws SQLException, MiExcepcion {
+        conectar();
         if (existeUsuari(u)) {
             throw new MiExcepcion("ERROR: Ya existe un usuario con este nombre");
         } else {
@@ -49,7 +49,8 @@ public void insertarUser(Usuari u) throws SQLException, MiExcepcion {
     }
 
     public void insertarPlayer(Player p) throws SQLException, MiExcepcion {
-    conectar();
+
+        conectar();
         if (existePlayer(p)) {
             throw new MiExcepcion("ERROR: Ya existe un player con ese nombre");
         } else {
@@ -72,7 +73,9 @@ public void insertarUser(Usuari u) throws SQLException, MiExcepcion {
             desconectar();
         }
     }
-public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
+
+    public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
+        conectar();
         if (existeEquipo(e)) {
             throw new MiExcepcion("ERROR: Ya existe un equipo con ese nombre");
         } else {
@@ -81,7 +84,7 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
             // Necesitamos preparar la consulta parametrizada
             PreparedStatement ps = conexion.prepareStatement(insert);
             // Le damos valor a los interrogantes
-            
+
             ps.setString(1, e.getNom());
             ps.setString(2, e.getCiutat());
             ps.setInt(3, e.getTelefon());
@@ -91,21 +94,26 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
             ps.executeUpdate();
             // cerramos recursos
             ps.close();
+            desconectar();
         }
     }
 // hi ha alguna cosa malament en la consulta sql crec
-         private void insertPartido(Partit equip1, Partit equip2) throws SQLException {
-             
+
+    private void insertPartido(Partit equip1, Partit equip2) throws SQLException {
+        conectar();
+
         String insert = "insert into partit (equip1,equip2) values (?,?,?,?,?)";
         PreparedStatement ps = conexion.prepareStatement(insert);
-        ps.setObject(1,equip1.getEquip1());
-        ps.setObject(2,equip2.getEquip2());
+        ps.setObject(1, equip1.getEquip1());
+        ps.setObject(2, equip2.getEquip2());
         ps.executeUpdate();
         ps.close();
+        desconectar();
     }
 
- // ********************* Funciones auxiliares ****************************
+    // ********************* Funciones auxiliares ****************************
     private boolean existePlayer(Player p) throws SQLException {
+        conectar();
         String select = "select * from player where nom='" + p.getNom() + "'";
         Statement st = conexion.createStatement();
         boolean existe = false;
@@ -115,10 +123,12 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
         }
         rs.close();
         st.close();
+        desconectar();
         return existe;
     }
-    
-     private boolean existeEquipo(Equip e) throws SQLException {
+
+    private boolean existeEquipo(Equip e) throws SQLException {
+        conectar();
         String select = "select * from equip where nom='" + e.getNom() + "'";
         Statement st = conexion.createStatement();
         boolean existe = false;
@@ -128,11 +138,12 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
         }
         rs.close();
         st.close();
+        desconectar();
         return existe;
     }
-     
+
     private boolean existeUsuari(Usuari e) throws SQLException {
-  
+        conectar();
         String select = "select * from usuari where username='" + e.getUsername() + "'";
         Statement st = conexion.createStatement();
         boolean existe = false;
@@ -142,12 +153,14 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
         }
         rs.close();
         st.close();
+        desconectar();
         return existe;
-        
+
     }
-     
-          private boolean existePartit(Partit p) throws SQLException {
-        String select = "select * from partit where Idpartit='" + p.getIdpartit()+ "'";
+
+    private boolean existePartit(Partit p) throws SQLException {
+        conectar();
+        String select = "select * from partit where Idpartit='" + p.getIdpartit() + "'";
         Statement st = conexion.createStatement();
         boolean existe = false;
         ResultSet rs = st.executeQuery(select);
@@ -156,60 +169,61 @@ public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
         }
         rs.close();
         st.close();
+        desconectar();
         return existe;
     }
-     
-    public String validateUserByPassword(String username, String contrasenya)throws SQLException, MiExcepcion {
-       conectar();
-        String select = "select * from usuari where username = '"+username+"' and contrasenya = '"+contrasenya+"' ";
-     Statement st = conexion.createStatement();
-     String validate = "false";
+
+    public String validateUserByPassword(String username, String contrasenya) throws SQLException, MiExcepcion {
+        conectar();
+        String select = "select * from usuari where username = '" + username + "' and contrasenya = '" + contrasenya + "' ";
+        Statement st = conexion.createStatement();
+        String validate = "false";
         ResultSet rs = st.executeQuery(select);
         if (rs.next()) {
             validate = "true";
         }
         rs.close();
         st.close();
-         desconectar();
+        desconectar();
         return validate;
-    }  
-    
-      public Usuari getUserByName(Usuari a)throws SQLException, MiExcepcion {
-      conectar();
-      Usuari aux = new Usuari();
-      aux.setUsername(a.getUsername());
-      if (!existeUsuari(aux)){
-          throw new MiExcepcion("No existe un usuario con ese nombre");
-      } else { 
-          String select = "select * from usuari where username='" + a.getUsername() + "'";
-          Statement st = conexion.createStatement();
-          ResultSet rs = st.executeQuery(select);
-          if(rs.next()){
-            a.setUsername(a.getUsername());
-            a.setContrasenya(rs.getString("contrasenya"));
-            a.setTipo(rs.getInt("tipo"));
-            
-          }
-          rs.close();
-          st.close();
-          desconectar();
-          return a;
     }
-      }
+
+    public Usuari getUserByName(Usuari a) throws SQLException, MiExcepcion {
+        conectar();
+        Usuari aux = new Usuari();
+        aux.setUsername(a.getUsername());
+        if (!existeUsuari(aux)) {
+            throw new MiExcepcion("No existe un usuario con ese nombre");
+        } else {
+            String select = "select * from usuari where username='" + a.getUsername() + "'";
+            Statement st = conexion.createStatement();
+            ResultSet rs = st.executeQuery(select);
+            if (rs.next()) {
+                a.setUsername(a.getUsername());
+                a.setContrasenya(rs.getString("contrasenya"));
+                a.setTipo(rs.getInt("tipo"));
+
+            }
+            rs.close();
+            st.close();
+            desconectar();
+            return a;
+        }
+    }
 //********************* Deletes****************************
-      
-public void DeleteUser(Usuari e) throws SQLException, MiExcepcion {
-    conectar();
+
+    public void DeleteUser(Usuari e) throws SQLException, MiExcepcion {
+        conectar();
         if (!existeUsuari(e)) {
             throw new MiExcepcion("ERROR: no existe user");
         } else {
             // Definimos la consulta
-            String delete = "delete from user where username = "+ e.getUsername();
+            String delete = "delete from user where username = " + e.getUsername();
             // Necesitamos preparar la consulta parametrizada
             PreparedStatement ps = conexion.prepareStatement(delete);
             // Le damos valor a los interrogantes
             System.out.println("Se ha eliminado correctamente");
-            
+
             // Ejecutamos la consulta
             ps.executeUpdate(delete);
             // cerramos recursos
@@ -217,34 +231,30 @@ public void DeleteUser(Usuari e) throws SQLException, MiExcepcion {
             desconectar();
         }
     }
-      
-      
-      
-      
-      //********************* Selects ****************************
-     
-     //********************* Update ****************************
-     public void modificarUserUsername(Usuari user, String newUsername) throws SQLException {
+
+    //********************* Selects ****************************
+    //********************* Update ****************************
+    public void modificarUserUsername(Usuari user, String newUsername) throws SQLException {
         Statement st = conexion.createStatement();
         String updateUserUsername = "update usuari set username = '" + newUsername + "' where username = '" + user.getUsername() + "'";
         st.executeUpdate(updateUserUsername);
         System.out.println("Se ha modificado correctamente el nombre del usuario " + user.getUsername());
         st.close();
+        desconectar();
 
     }
-     
-       public void modificarPlayerTeam(Player player, String newTeam) throws SQLException {
+
+    public void modificarPlayerTeam(Player player, String newTeam) throws SQLException {
         Statement st = conexion.createStatement();
         String updateTeam = "update player set equip = '" + newTeam + "' where equip = '" + player.getEquip() + "'";
         st.executeUpdate(updateTeam);
         System.out.println("Se ha modificado correctamente el nombre del usuario " + player.getEquip());
         st.close();
+        desconectar();
 
     }
-     
 
-
-       // ********************* Conectar / Desconectar ****************************
+    // ********************* Conectar / Desconectar ****************************
     public void conectar() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/futsal";
         String user = "root";
