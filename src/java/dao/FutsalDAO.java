fe2fbfbb1d0f01257ce727517fc18dbbf168867f
asg97 <serrano.ag6@gmail.com>
2018-05-15 09:57:58 +0200

    implementado update de username en user y change player team(idk if it work) y insert team and partit

--- src/java/dao/FutsalDAO.java
+++ src/java/dao/FutsalDAO.java
@@ -49,6 +49,35 @@ public void insertarPlayer(Player p) throws SQLException, MiExcepcion {
             ps.close();
         }
     }
+public void insertarTeam(Equip e) throws SQLException, MiExcepcion {
+        if (existeEquipo(e)) {
+            throw new MiExcepcion("ERROR: Ya existe un equipo con ese nombre");
+        } else {
+            // Definimos la consulta
+            String insert = "insert into equip values (?, ?, ?, ?, ?, ?, ?)";
+            // Necesitamos preparar la consulta parametrizada
+            PreparedStatement ps = conexion.prepareStatement(insert);
+            // Le damos valor a los interrogantes
+            
+            ps.setString(1, e.getNom());
+            ps.setString(3, e.getCiutat());
+            ps.setInt(4, e.getTelefon());
+            ps.setInt(5, e.getPunts());
+            ps.setInt(6, e.getUser());
+            // Ejecutamos la consulta
+            ps.executeUpdate();
+            // cerramos recursos
+            ps.close();
+        }
+    }
+         private void insertPartido(Partit equip1, Partit equip2) throws SQLException {
+        String insert = "insert into partit (equip1,equip2) values (?,?,?,?,?)";
+        PreparedStatement ps = conexion.prepareStatement(insert);
+        ps.setObject(1,equip1.getEquip1());
+        ps.setObject(2,equip2.getEquip2());
+        ps.executeUpdate();
+        ps.close();
+    }
 
  // ********************* Funciones auxiliares ****************************
     private boolean existePlayer(Player p) throws SQLException {
@@ -63,6 +92,43 @@ public void insertarPlayer(Player p) throws SQLException, MiExcepcion {
         st.close();
         return existe;
     }
+     private boolean existeEquipo(Equip e) throws SQLException {
+        String select = "select * from equip where nom='" + e.getNom() + "'";
+        Statement st = conexion.createStatement();
+        boolean existe = false;
+        ResultSet rs = st.executeQuery(select);
+        if (rs.next()) {
+            existe = true;
+        }
+        rs.close();
+        st.close();
+        return existe;
+    }
+     
+
+      // ********************* Selects ****************************
+     
+     // ********************* Update ****************************
+     public void modificarUserName(Usuari user, String newUsername) throws SQLException {
+        Statement st = conexion.createStatement();
+        String updateUsername = "update usuari set username = '" + newUsername + "' where username = '" + user.getUsername() + "'";
+        st.executeUpdate(updateUsername);
+        System.out.println("Se ha modificado correctamente el nombre del usuario " + user.getUsername());
+        st.close();
+
+    }
+     
+       public void modificarPlayerTeam(Player player, String newTeam) throws SQLException {
+        Statement st = conexion.createStatement();
+        String updateTeam = "update player set equip = '" + newTeam + "' where equip = '" + player.getEquip() + "'";
+        st.executeUpdate(updateTeam);
+        System.out.println("Se ha modificado correctamente el nombre del usuario " + player.getEquip());
+        st.close();
+
+    }
+     
+     
+     
 
        // ********************* Conectar / Desconectar ****************************
     public void conectar() throws SQLException {
